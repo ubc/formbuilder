79fa87618c92250605e78103047fd369418d8b91
John Hsu <john.hsu@ubc.ca>
2013-04-08 17:20:35 -0700

    Fixed position nav & question panel, iconify nav.
    Making the question bank fixed position should be more convenient for
    adding questions as users can just scroll to the question they want, and
    then scroll to the section of the form they want, then just drag and
    drop the questions in. It also fits well with the fixed position top
    navbar.
    
    Note that because there's no way for css to properly deal with fixed
    position in a responsive manner and because bootstrap won't let me mix
    fixed width and responsive width columns, I had to style my own fixed
    width sidebar with a responsive main content area.
    
    The print button was moved to the top navbar. The top navbar now has
    icons instead of text buttons.

--- src/Meot/FormBundle/Resources/views/Default/index.html.php
+++ src/Meot/FormBundle/Resources/views/Default/index.html.php
@@ -10,15 +10,13 @@
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width">
 
-        <link rel="stylesheet" href="css/bootstrap.min.css">
         <style>
             body {
-                padding-top: 60px;
-                padding-bottom: 40px;
+                padding-top: 42px;
             }
         </style>
         <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.1/themes/base/jquery-ui.css" />
-        <link rel="stylesheet" href="css/bootstrap-responsive.min.css">
+        <link rel="stylesheet" href="css/bootstrap.css">
         <link rel="stylesheet" href="css/font-awesome.min.css">
         <!--[if IE 7]>
         <link rel="stylesheet" href="css/font-awesome-ie7.min.css">
@@ -31,17 +29,16 @@
 
         <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
 
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
-        <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-ui-1.10.1.min.js"><\/script>')</script>
+        <!-- Non-minified versions during development -->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.js"></script>
 
-        <script src="js/vendor/bootstrap.min.js"></script>
+        <script src="js/vendor/bootstrap.js"></script>
         <script src="js/vendor/chardinjs.min.js"></script>
 
         <script src="js/main.js"></script>
-        <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.0.5/angular.min.js"></script>
-        <script src="http://code.angularjs.org/1.0.5/angular-resource.min.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.0.5/angular.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.0.5/angular-resource.js"></script>
         <script src="js/vendor/angular-ui.min.js"></script>
         <script src="js/vendor/ui-bootstrap-tpls-0.2.0.min.js"></script>
         <script src="js/vendor/underscore-min.js"></script>
@@ -59,73 +56,64 @@
             <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
         <![endif]-->
 
+        <!-- Top Navbar -->
         <div class="navbar navbar-inverse navbar-fixed-top">
             <div class="navbar-inner">
                 <div class="container-fluid">
-                    <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+                    <!-- Left aligned elements -->
                     <a class="brand" href="#">Form Builder</a>
-                    <div class="nav-collapse collapse" ng-controller="MenuCtrl">
-                        <p class="navbar-text pull-right">
-                            Logged in as <a href="#" class="navbar-link">Username</a>&nbsp;&nbsp;
-                            <a href="#">Logout</a>
-                        </p>
-                        <ul class="nav">
-                            <li><a href ng-click="new()">New</a></li>
-                            <li><a href ng-click="save()">Save</a></li>
-                            <li><a href ng-click="load()">Load</a></li>
-                            <li><a href ng-click="help()">Help</a></li>
-                        </ul>
-                    </div><!--/.nav-collapse -->
+                    <ul class="nav" ng-controller="MenuCtrl">
+                        <li><a title="New Form" href="#" ng-click="new()"><i class="icon-file icon-large"></i></a></li>
+                        <li><a title="Save Form" href="#" ng-click="save()"><i class="icon-save icon-large"></i></a></li>
+                        <li><a title="Load Form" href="#" ng-click="load()"><i class="icon-folder-open icon-large"></i></a></li>
+                        <li><a title="Print Form" href="#" onclick="window.print();"><i class="icon-print icon-large"></i></a></li>
+                        <li><a title="Shwo Help" href="#" ng-click="help()"><i class="icon-question-sign icon-large"></i></a></li>
+                    </ul>
+                    <!-- Right aligned elements -->
+                    <ul class="nav pull-right">
+                        <li><a href="#"><i class="icon-user"></i>Username</a></li>
+                        <li><a href="#"><i class="icon-signout icon-large"></i></a></li>
+                    </ul>
                 </div>
             </div>
         </div>
 
-        <div class="container-fluid">
-            <div class="row-fluid">
-                <div class="span3" id="panel">
-                    <div class="well sidebar-nav" ng-view data-intro="Question bank, where you can drag questions to your form." data-position="bottom">
-
-                    </div><!--/.well -->
-                </div><!--/span-->
-                <div class="span9" ng-controller="FormCtrl">
-                    <h2 class="editable pull-left" contenteditable="true" ng-model="form.name" data-intro="Form name" data-position="right"></h2>
-                    <button id="printBtn" class="btn pull-right" onclick="window.print();"><i class="icon-print">Print</i></button>
-                    <div class="clearfix"></div>
-                    <div class="hero-unit" id="form-paper" droppable drop="addQuestion(id)">
-                        <div class="editable" contenteditable="true" ng-model="form.header" data-intro="Form header" data-position="bottom"></div>
-                        <ul id="question-list" ui-sortable="sortableOptions" ng-model="form.questions" editable-ul>
-                            <li resizable='{containment: "parent"}' ng-repeat="question in form.questions" ng-switch="question.response_type" ng-style="question.metadata" ng-model="question">
-                                <div class="handle pull-left"><i class="icon-arrow-up"></i><br/><i class="icon-arrow-down"></i></div>
-                                <div class="editable" contenteditable="true" ng-model="question.text"></div>
-                                <div ng-switch-when="1" class="response-input"><input type="text"/></div>
-                                <div ng-switch-when="2" class="response-textarea"><textarea storable ng-style="question.response_metadata"></textarea></div>
-                                <div ng-switch-when="3" class="response-radio">
-                                    <label class="radio" ng-class="response.classes" ng-repeat="response in question.responses">
-                                        <input type="radio" disabled><span class="editable" contenteditable="true" ng-model="response.text"></span>
-                                    </label>
-                                </div>
-                                <div ng-switch-when="4" class="response-checkbox">
-                                    <label class="checkbox" ng-class="response.classes" ng-repeat="response in question.responses">
-                                        <input type="checkbox" disabled><span class="editable" contenteditable="true" ng-model="response.text"></span>
-                                    </label>
-                                </div>
-                            </li>
-                        </ul>
-                        <div class="editable" contenteditable="true" ng-model="form.footer" data-intro="Form footer" data-position="bottom"></div>
-                    </div>
-                    {{form}}
-                </div><!--/span-->
-            </div><!--/row-->
-
+        <!-- Sidebar -->
+        <div id="panel" ng-view data-intro="Question bank, where you can drag questions to your form." data-position="bottom">
+            <p>question question question question question question</p>
+        </div>
+        <!-- Main Content -->
+        <div id="formcontent" ng-controller="FormCtrl">
+            <h2 class="editable pull-left" contenteditable="true" ng-model="form.name" data-intro="Form name" data-position="right"></h2>
+            <div class="clearfix"></div>
+            <div class="hero-unit" id="form-paper" droppable drop="addQuestion(id)">
+                <div class="editable" contenteditable="true" ng-model="form.header"  data-intro="Form header" data-position="bottom"></div>
+                <ul id="question-list" ui-sortable="sortableOptions" ng-model="form.questions" editable-ul>
+                    <li resizable='{containment: "parent"}' ng-repeat="question in form.questions" ng-switch="question.response_type" ng-style="question.metadata" ng-model="question">
+                        <button ng-click="delete($index)"><i class="icon-remove"></i></button>
+                        <div class="handle pull-left"><i class="icon-arrow-up"></i><br/><i class="icon-arrow-down"></i></div>
+                        <div class="editable" contenteditable="true" ng-model="question.text"></div>
+                        <div ng-switch-when="1" class="response-input"><input type="text"/></div>
+                        <div ng-switch-when="2" class="response-textarea"><textarea storable ng-style="question.response_metadata" class="responseText"></textarea></div>
+                        <div ng-switch-when="3" class="response-radio">
+                            <label class="radio" ng-class="response.classes" ng-repeat="response in question.responses">
+                                <input type="radio" disabled><span class="editable" contenteditable="true" ng-model="response.text"></span>
+                            </label>
+                        </div>
+                        <div ng-switch-when="4" class="response-checkbox">
+                            <label class="checkbox" ng-class="response.classes" ng-repeat="response in question.responses">
+                                <input type="checkbox" disabled><span class="editable" contenteditable="true" ng-model="response.text"></span>
+                            </label>
+                        </div>
+                    </li>
+                </ul>
+                <div class="editable" contenteditable="true" ng-model="form.footer"  data-intro="Form footer" data-position="bottom"></div>
+            </div>
+            {{form}}
             <footer>
             <hr>
                 <p>&copy; <a href="http://ctlt.ubc.ca/">Centre for Teaching, Learning and Technology, UBC</a> 2013</p>
             </footer>
-
-        </div><!--/.fluid-container-->
+        </div><!--/formpanel-->
     </body>
 </html>
