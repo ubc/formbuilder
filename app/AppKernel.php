5d5f020668c9e2355e815c6f246394d685f82aca
Compass <pan.luo@ubc.ca>
2013-07-03 12:16:54 -0700

    Add Shib required files and configs

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -24,6 +24,7 @@ class AppKernel extends Kernel
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new KULeuven\ShibbolethBundle\ShibbolethBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
