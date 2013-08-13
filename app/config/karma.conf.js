12f42c6e2250f65d6e0d7d5c312e57db0742bb1c
Compass <pan.luo@ubc.ca>
2013-08-12 17:27:44 -0700

    Update karma config to work with karma 0.10.1

--- app/config/karma.conf.js
+++ app/config/karma.conf.js
@@ -1,21 +1,27 @@
-basePath = '../../';
+module.exports = function(config) {
+    config.set({
+        basePath: '../../',
 
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'web/js/vendor/angular.js',
-  'web/js/vendor/angular-*.js',
-  'test/lib/angular/angular-mocks.js',
-  'web/js/vendor/jquery*.js',
-  'web/js/**/*.js',
-  'test/unit/**/*.js'
-];
+        files: [
+            'web/js/vendor/angular.js',
+            'web/js/vendor/angular-*.js',
+            'test/lib/angular/angular-mocks.js',
+            'web/js/vendor/jquery*.js',
+            'web/js/**/*.js',
+            'test/unit/**/*.js'
+        ],
 
-autoWatch = true;
+        autoWatch: true,
 
-browsers = ['Chrome'];
+        browsers: ['Chrome'],
 
-junitReporter = {
-  outputFile: 'js_unit.xml',
-  suite: 'unit'
+        frameworks: ["jasmine"],
+
+        junitReporter: {
+            outputFile: 'js_unit.xml',
+            suite: 'unit'
+        },
+
+        reportSlowerThan: 500,
+    });
 };
--- app/config/karma_ci.conf.js
+++ app/config/karma_ci.conf.js
@@ -1,23 +1,28 @@
-basePath = '../../';
+module.exports = function(config) {
+    config.set({
+        basePath: '../../',
 
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'web/js/vendor/angular.js',
-  'web/js/vendor/angular-*.js',
-  'test/lib/angular/angular-mocks.js',
-  'web/js/vendor/jquery*.js',
-  'web/js/**/*.js',
-  'test/unit/**/*.js'
-];
+        files: [
+            'web/js/vendor/angular.js',
+            'web/js/vendor/angular-*.js',
+            'test/lib/angular/angular-mocks.js',
+            'web/js/vendor/jquery*.js',
+            'web/js/**/*.js',
+            'test/unit/**/*.js'
+        ],
 
-autoWatch = false;
+        autoWatch: false,
 
-singleRun = true;
-reporters = ['dots', 'junit'];
-browsers = ['PhantomJS', 'Firefox'];
+        singleRun: true,
+        reporters: ['dots', 'junit'],
+        browsers: ['PhantomJS', 'Firefox'],
+        frameworks: ["jasmine"],
 
-junitReporter = {
-  outputFile: 'app/build/logs/js_unit.xml',
-  suite: 'unit'
+        junitReporter: {
+            outputFile: 'app/build/logs/js_unit.xml',
+            suite: 'unit'
+        },
+
+        reportSlowerThan: 500,
+    });
 };
